d90b1263c94d263174271e9a35f8474395decdb1
Sarsenov <zhanbek.sarsenov@gmail.com>
2026-02-15 02:39:54 +0500

    Покращує код

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,19 @@ public class Main {
     private static final String FILE_PATH = "src/myFile.txt";
     public static void main(String[] args) {
 
-        try (FileWriter fileWriter = new FileWriter(FILE_PATH)) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+             FileWriter fileWriter = new FileWriter(FILE_PATH)) {
+
             System.out.println();
-            System.out.println("Введіть у консоль дані, які потрібно передати у файл: " + FILE_PATH);
-            System.out.println("Для завершения ввода данных введите строку \"Stop\": ");
+            System.out.println("Введіть дані (для завершення введіть \"Stop\"):");
             System.out.println();
 
-            String line  = bufferedReader.readLine();
-            while(!line.equalsIgnoreCase("Stop")){
-                fileWriter.write(line);
-                fileWriter.write("\n");
-                line  = bufferedReader.readLine();
+            String line;
+            while(!(line = bufferedReader.readLine()).equalsIgnoreCase("Stop")) {
+                fileWriter.write(line + System.lineSeparator());
             }
             System.out.println();
             System.out.println("Всі введені в консоль данние успішно передані в файл: " + FILE_PATH);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -30,13 +26,11 @@ public class Main {
         System.out.println("А тепер виведемо дані з файлу " + FILE_PATH);
         System.out.println();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(FILE_PATH)))) {
-            String line  = bufferedReader.readLine();
-            while (line  != null) {
+            String line;
+            while ((line  = bufferedReader.readLine()) != null) {
                 System.out.println(line);
-                line  = bufferedReader.readLine();
+
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
